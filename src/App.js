62944353be1a24851a49487d95cce0bbbbba1718
kzollove <kylezollo@gmail.com>
2020-02-01 12:37:18 -0400

    initialize

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar.component"
+
 
 function App() {
   return (
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={ Main } />
+      <Route path="/site-map" component={ SiteMap } />
+      <Route path="/site-list" component={ SiteList } />
+      <Route path="/site-create" component={ CreateSite } />
+      <Route path="/site-edit/:id" component={ EditSite } />
+
+      <Route path="/deploy" component={ DeploySonde } />
+      <Route path="/hobolog" component={ HoboLog } />
+
+
+    </Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
