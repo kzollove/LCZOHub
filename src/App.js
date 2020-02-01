ee6ee049c4bc32c6a6bac5fd4154aadbb7b813c5
kzollove <kylezollo@gmail.com>
2020-02-01 13:11:44 -0400

    Added Navbar from ReactStrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 
-import Navbar from "./components/navbar.component"
+import Navbar from "./components/navbar.component";
+import Main from "./components/main.component"
 
 
 function App() {
@@ -11,18 +12,16 @@ function App() {
       <Navbar />
       <br />
       <Route path="/" exact component={ Main } />
-      <Route path="/site-map" component={ SiteMap } />
+      {/* <Route path="/site-map" component={ SiteMap } />
       <Route path="/site-list" component={ SiteList } />
       <Route path="/site-create" component={ CreateSite } />
       <Route path="/site-edit/:id" component={ EditSite } />
 
       <Route path="/deploy" component={ DeploySonde } />
-      <Route path="/hobolog" component={ HoboLog } />
+      <Route path="/hobolog" component={ HoboLog } /> */}
 
 
     </Router>
-    <div className="App">
-    </div>
   );
 }
 
